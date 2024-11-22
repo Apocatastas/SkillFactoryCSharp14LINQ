5a5333a52d35571483fdc1b51435395ce9997f86
ɐllɐ <molchanova.a.a@gmail.com>
2024-11-22 21:19:28 +0300

    task 8 part 2

--- SkillFactoryCSharp14LINQ/Program.cs
+++ SkillFactoryCSharp14LINQ/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace SkillFactoryCSharp14LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objects = new List<object>()
+            {
+               1,
+               "Сергей ",
+               "Андрей ",
+               300,
+            };
+
+            foreach (var stringObject in objects.Where(o => o is string).OrderBy(o => o))
+                Console.WriteLine(stringObject);
+        }
+    }
+}
