c3da4680c1030470cdaae8c3d3d6f3f4395b4614
ɐllɐ <molchanova.a.a@gmail.com>
2024-11-24 21:38:49 +0300

    task 14.1.2

--- SkillFactoryCSharp14LINQ/Program.cs
+++ SkillFactoryCSharp14LINQ/Program.cs
@@ -7,23 +7,20 @@ namespace SkillFactoryCSharp14LINQ
     class Program
     {
         /// <summary>
-        /// А теперь попробуйте выбрать все города, название у которых не длиннее 10 букв,
-        /// и отсортируйте их по длине названия.
-        /// </summary>
+        /// Соедините все слова в одну последовательность
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            var russianCities = new List<City>();
-            russianCities.Add(new City("Москва", 11900000));
-            russianCities.Add(new City("Санкт-Петербург", 4991000));
-            russianCities.Add(new City("Волгоград", 1099000));
-            russianCities.Add(new City("Казань", 1169000));
-            russianCities.Add(new City("Севастополь", 449138));
-
-            var bigCities = russianCities.Where(c => c.Name.Length <= 10).OrderByDescending(c => c.Name.Length);
-
-            foreach (var bigCity in bigCities)
-                Console.WriteLine(bigCity.Name + " - " + bigCity.Population);
+            string[] text = { "Раз два три",
+                              "четыре пять шесть",
+                              "семь восемь девять" };
+            var splittedText = from str in text 
+                        from word in str.Split(' ') 
+                        select word;
+            foreach (var word in splittedText)
+            {
+                Console.WriteLine(word);
+            }
             Console.ReadKey();
         }
     }
