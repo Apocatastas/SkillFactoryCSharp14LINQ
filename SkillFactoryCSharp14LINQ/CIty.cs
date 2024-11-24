fee072cc8f2b4802dcbb17859a4052e3be4303e5
ɐllɐ <molchanova.a.a@gmail.com>
2024-11-24 21:17:24 +0300

    task 14.1.1

---
+++ SkillFactoryCSharp14LINQ/CIty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SkillFactoryCSharp14LINQ
+{
+	public class City
+	{
+        public City(string name, long population)
+        {
+            Name = name;
+            Population = population;
+        }
+
+        public string Name { get; set; }
+        public long Population { get; set; }
+    }
+}
+
--- SkillFactoryCSharp14LINQ/Program.cs
+++ SkillFactoryCSharp14LINQ/Program.cs
@@ -6,18 +6,25 @@ namespace SkillFactoryCSharp14LINQ
 {
     class Program
     {
+        /// <summary>
+        /// А теперь попробуйте выбрать все города, название у которых не длиннее 10 букв,
+        /// и отсортируйте их по длине названия.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            var objects = new List<object>()
-            {
-               1,
-               "Сергей ",
-               "Андрей ",
-               300,
-            };
+            var russianCities = new List<City>();
+            russianCities.Add(new City("Москва", 11900000));
+            russianCities.Add(new City("Санкт-Петербург", 4991000));
+            russianCities.Add(new City("Волгоград", 1099000));
+            russianCities.Add(new City("Казань", 1169000));
+            russianCities.Add(new City("Севастополь", 449138));
 
-            foreach (var stringObject in objects.Where(o => o is string).OrderBy(o => o))
-                Console.WriteLine(stringObject);
+            var bigCities = russianCities.Where(c => c.Name.Length <= 10).OrderByDescending(c => c.Name.Length);
+
+            foreach (var bigCity in bigCities)
+                Console.WriteLine(bigCity.Name + " - " + bigCity.Population);
+            Console.ReadKey();
         }
     }
 }
