4210c6477d0a7156df819f186c87e52a6cc378fd
ɐllɐ <molchanova.a.a@gmail.com>
2024-11-26 21:30:19 +0300

    task 14.2.4

---
+++ SkillFactoryCSharp14LINQ/Coarse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SkillFactoryCSharp14LINQ
+{
+	public class Coarse
+	{
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
+
--- SkillFactoryCSharp14LINQ/Program.cs
+++ SkillFactoryCSharp14LINQ/Program.cs
@@ -1,38 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace SkillFactoryCSharp14LINQ
 {
     class Program
     {
         /// <summary>
-        /// Выберите всех студентов моложе 27,
-        /// сгенерируйте из них анкеты.
+        /// Теперь добавьте всех студентов младше 29 лет,
+        /// владеющих английским языком, в курс «Язык программирования C#». 
+        ///Новая анонимная сущность для выборки должна иметь следующие поля:
+        /// имя(строка);
+        /// год рождения студента(целое число);
+        /// имя курса(строка).
         /// </summary>
         /// <param name="args"></param>
+
         static void Main(string[] args)
         {
-            List<Student> students = new List<Student>
-                                            {
-                                                new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
-                                                new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
-                                                new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
-                                                new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
-                                            };
+            // Список студентов
+            var students = new List<Student>
+                            {
+                                new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+                                new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
+                                new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }}
+                            };
 
-            var youngStudentApplications = from s in students
-                                           where s.Age < 27 
-                                           let birthYear = DateTime.Now.Year - s.Age
-                                           select new Application()
-                                           {
-                                               Name = s.Name,
-                                               YearOfBirth = birthYear
-                                           };
-            foreach (var studApplication in youngStudentApplications)
+            // Список курсов
+            var coarses = new List<Coarse>
+                            {
+                                new Coarse {Name="Язык программирования C#", StartDate = new DateTime(2020, 12, 20)},
+                                new Coarse {Name="Язык SQL и реляционные базы данных", StartDate = new DateTime(2020, 12, 15)},
+                            };
+        
+            var studentsWithCoarses = from stud in students
+                                      where stud.Age < 29
+                                      where stud.Languages.Contains("английский") 
+                                      let birthYear = DateTime.Now.Year - stud.Age
+                                      from coarse in coarses
+                                      where coarse.Name.Contains("C#")
+                                      select new
+                                      {
+                                          Name = stud.Name,
+                                          BirthYear = birthYear,
+                                          CoarseName = coarse.Name
+                                      };
+            foreach (var stud in studentsWithCoarses)
             {
-                Console.WriteLine(studApplication.Name + ", " + studApplication.YearOfBirth);
-            }
+                Console.WriteLine($"Студент {stud.Name} ({stud.BirthYear}) добавлен курс {stud.CoarseName}");
+            };
                 
             Console.ReadKey();
 
